604ffb031bbdd8b2672d2608e5c687d3127a1001
ROUSE-prog <stevenr2049@gmail.com>
2024-07-21 16:10:28 -0400

    controllers

--- controllers/petsController.js
+++ controllers/petsController.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createPet, getAllPets, getPet } = require('../queries/pet');
+const { createPet, getAllPets, getPet, updatePet, deletePet } = require('../queries/pet');
 
 // Get all pets
 router.get('/', async (req, res) => {
@@ -18,9 +18,13 @@ router.get('/:id', async (req, res) => {
   const { id } = req.params;
   try {
     const pet = await getPet(id);
+    if (!pet) {
+      return res.status(404).json({ error: `No pet with the id ${id} exists` });
+    }
     res.status(200).json(pet);
   } catch (error) {
-    res.status(404).json({ error: `No color with the id ${id} exists` });
+    console.error(error);
+    res.status(500).json({ error: 'Failed to fetch pet' });
   }
 });
 
@@ -32,6 +36,36 @@ router.post('/', async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Failed to create pet' });
+  }
+});
+
+// Update a single pet
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const updatedPet = await updatePet(id, req.body);
+    if (!updatedPet) {
+      return res.status(404).json({ msg: 'Pet not found' });
+    }
+    res.json(updatedPet);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// Delete a single pet
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedPet = await deletePet(id);
+    if (!deletedPet) {
+      return res.status(404).json({ msg: 'Pet not found' });
+    }
+    res.json({ msg: 'Pet deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
   }
 });
 
--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,8 +1,8 @@
 const pgp = require('pg-promise')();
 require('dotenv').config();
 
-const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER, PG_PASSWORD } =
-  process.env;
+const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER, PG_PASSWORD } = process.env;
+
 const cn = DATABASE_URL
   ? {
       connectionString: DATABASE_URL,
@@ -13,6 +13,7 @@ const cn = DATABASE_URL
       port: PG_PORT,
       database: PG_DATABASE,
       user: PG_USER,
+      password: PG_PASSWORD,
     };
 
 const db = pgp(cn);
