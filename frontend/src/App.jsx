9c19aadb2589ed15de2fa06708ea0847673fa4fc
alexandra-perez <alexandraperezny@gmail.com>
2024-07-22 19:16:34 -0400

    Add app.jsx setup for Nav and Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
 import { useState } from 'react';
+import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
 import './App.css';
 
 function App() {
-  return <></>;
+  return (
+    <div className="Wrapper">
+      <Navbar />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
