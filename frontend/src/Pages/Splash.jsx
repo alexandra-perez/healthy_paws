de7343fe97685b54b112e9ca92269f62d2562d4a
alexandra-perez <alexandraperezny@gmail.com>
2024-07-22 20:10:06 -0400

    Add routes for Splash and Index

--- frontend/src/Pages/Splash.jsx
+++ frontend/src/Pages/Splash.jsx
@@ -6,7 +6,6 @@ import Footer from '../Components/Footer/Footer';
 const Splash = () => {
   return (
     <div>
-      <NavBar />
       <Hero />
       <div className="splash-content">
         {/* Add any additional content or components here */}
