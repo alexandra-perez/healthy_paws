4f8363d38bd53c09345e31df97ada3d7369bcc1e
ROUSE-prog <stevenr2049@gmail.com>
2024-07-23 22:13:56 -0400

    light maintaince

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -7,14 +7,14 @@ const Hero = () => {
   return (
     <div className="hero">
       <div className="image-container">
-        <h1>Welcome to Petcare</h1>
+        <h1>Welcome to HealthyPaws</h1>
         <p>Your one-stop solution for all pet care needs.</p>
         <SearchBar />
       </div>
       <div className="top-info-container">
         <h2>Comprehensive Health Tracking for Your Pet</h2>
         <p>
-          Petcare is your ultimate companion for managing your pet's health.
+          HealthyPaws is your ultimate companion for managing your pet's health.
           From age and weight to vaccination records, monitor all vital
           information with ease, just like you would track your own health.
           Ensure your furry friend stays healthy and happy!
@@ -25,7 +25,6 @@ const Hero = () => {
           Join our community of pet lovers and give your pet the care they
           deserve.
           <br />
-          <button>Get Started</button>
         </h2>
       </div>
     </div>
