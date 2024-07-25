46d6aad276c8daa5711561c4aae983d9f1b73144
ROUSE-prog <stevenr2049@gmail.com>
2024-07-24 21:58:46 -0400

    updates

--- frontend/src/Pages/Splash/Splash.jsx
+++ frontend/src/Pages/Splash/Splash.jsx
@@ -6,8 +6,12 @@ import Footer from '../../Components/Footer/Footer';
 const Splash = () => {
   return (
     <div>
+      <NavBar />
       <Hero />
       <div className="splash-content">
+        {/* Add any additional splash content here */}
+      </div>
+      <Footer />
     </div>
   );
 };
