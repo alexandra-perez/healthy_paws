2d1818fd3619b54d94b95737041eaa85bc2c4e4c
alexandra-perez <alexandraperezny@gmail.com>
2024-07-27 12:22:02 -0400

    Add confirmation modal for deleting a pet

--- frontend/src/Pages/ShowPet/ShowPet.jsx
+++ frontend/src/Pages/ShowPet/ShowPet.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import PetProfile from '../../Components/PetProfile/PetProfile';
+import './ShowPet.scss';
 
 export default function ShowPet() {
   const [currentPet, setCurrentPet] = useState({
@@ -12,6 +13,12 @@ export default function ShowPet() {
     isvaccinated: false,
     existingconditions: '',
   });
+  const [clicked, setClicked] = useState(false);
+
+  function showModal() {
+    setClicked((prevClick) => !prevClick);
+    console.log(setClicked);
+  }
 
   let { id } = useParams();
   const navigate = useNavigate();
@@ -49,7 +56,12 @@ export default function ShowPet() {
   return (
     <div className="ShowPet">
       <PetProfile currentPet={currentPet} setCurrentPet={setCurrentPet} />
-      <button onClick={handleDelete}>Delete Pet</button>
+      <div className={`delete-modal ${clicked ? 'visible' : 'hidden'}`}>
+        <p>Are you sure you want to delete this pet?</p>
+        <button onClick={handleDelete}>Confirm</button>
+        <button onClick={showModal}>Cancel</button>
+      </div>
+      <button onClick={showModal}>Delete Pet</button>
     </div>
   );
 }
