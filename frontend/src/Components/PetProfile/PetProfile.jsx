b813a0e42b02ea3b72532d8882e8ae5fbc7d67f6
alexandra-perez <alexandraperezny@gmail.com>
2024-07-27 13:24:32 -0400

    Add CSS for PetProfile

--- frontend/src/Components/PetProfile/PetProfile.jsx
+++ frontend/src/Components/PetProfile/PetProfile.jsx
@@ -1,19 +1,61 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import defaultImg from '../../assets/pet-default-image.png';
+
+import './PetProfile.scss';
 
 export default function PetProfile({ currentPet }) {
+  const [clicked, setClicked] = useState(false);
+
+  function showModal() {
+    setClicked((prevClick) => !prevClick);
+    console.log(setClicked);
+  }
+
+  function handleDelete() {
+    fetch(`${API}/pets/${id}`, {
+      method: 'DELETE',
+      body: JSON.stringify(currentPet),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(() => {
+        navigate('/index');
+      })
+      .catch((error) => console.error(error));
+  }
+
   return (
     <div className="PetProfile">
-      <div>
-        <h1>{currentPet.name}</h1>
-        <p>{currentPet.gender}</p>
-        <p>{currentPet.age}</p>
-        <p>{currentPet.weight} lbs</p>
-        <p>{currentPet.isvaccinated ? 'Vaccinated' : 'Not vaccinated'}</p>
-        <p>Existing Conditions: {currentPet.existingconditions}</p>
+      <h1>{currentPet.name}</h1>
+      <div className="wrapper">
+        <div className="img-container">
+          <img src={defaultImg} alt="" />
+        </div>
+        <div className="flex">
+          <div className="info-container">
+            <p>{currentPet.gender}</p>
+            <p>{currentPet.age}</p>
+            <p>{currentPet.weight} lbs</p>
+            <p>{currentPet.isvaccinated ? 'Vaccinated' : 'Not vaccinated'}</p>
+            <p>Existing Conditions: {currentPet.existingconditions}</p>
+          </div>
+          <div className="button-container">
+            <Link to={`/edit-pet/${currentPet.id}`}>
+              <button className="action-button">Edit</button>
+            </Link>
+            <button className="action-button" onClick={showModal}>
+              Delete
+            </button>
+          </div>
+        </div>
       </div>
-      <Link to={`/edit-pet/${currentPet.id}`}>
-        <button>Edit</button>
-      </Link>
+      <div className={`delete-modal ${clicked ? 'visible' : 'hidden'}`}>
+        <p>Are you sure you want to delete this pet?</p>
+        <button onClick={handleDelete}>Confirm</button>
+        <button onClick={showModal}>Cancel</button>
+      </div>
     </div>
   );
 }
