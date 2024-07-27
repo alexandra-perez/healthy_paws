30481a9c6fcd923abffba1761f9cfd64a3f1e515
alexandra-perez <alexandraperezny@gmail.com>
2024-07-27 15:33:11 -0400

    Add CSS to NavBar

--- frontend/src/Components/NavBar/Navbar.jsx
+++ frontend/src/Components/NavBar/Navbar.jsx
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import SearchBar from '../SearchBar/SearchBar';
 import './Navbar.scss';
+
 const Navbar = () => {
   const [showSearchBar, setShowSearchBar] = useState(false);
   const toggleSearchBar = () => {
     setShowSearchBar(!showSearchBar);
   };
+
   return (
     <nav className="navbar">
       <div className="navbar-brand">
@@ -17,7 +19,11 @@ const Navbar = () => {
         <Link to="/create-pet">Create Pet</Link>
         <Link to="/user-profile">User Profile</Link>
         <span className="navbar-search" onClick={toggleSearchBar}>
-          Search
+        <i
+            className={`fa-solid fa-magnifying-glass ${
+              showSearchBar ? 'block' : 'hidden'
+            }`}
+          ></i>
         </span>
       </div>
       {showSearchBar && <SearchBar />}
